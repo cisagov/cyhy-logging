6cab3396ae53f28a5fb5025669c82122e02ef8ee
Felddy <markf@geekpad.com>
2024-09-24 18:47:20 -0400

    Fix Rich tracebacks not installing correctly

--- src/cyhy_logging/cyhy_logging.py
+++ src/cyhy_logging/cyhy_logging.py
@@ -6,6 +6,7 @@ from typing import Optional
 
 # Third-Party Libraries
 from rich.logging import RichHandler
+from rich.traceback import install as install_rich_tracebacks
 
 from . import CYHY_ROOT_LOGGER
 from .log_filters import RedactPasswordFilter
@@ -17,19 +18,16 @@ def setup_logging(log_level: Optional[str] = None) -> None:
     if log_level:
         log_level = log_level.upper()
 
-    is_debug = log_level == logging.DEBUG
-
     logging.basicConfig(
         datefmt="[%X]",
+        force=True,
         format="%(message)s",
-        handlers=[
-            RichHandler(
-                rich_tracebacks=True,
-                show_path=is_debug,
-                tracebacks_show_locals=is_debug,
-            )
-        ],
+        handlers=[RichHandler()],
     )
+
+    # Install the Rich traceback handler
+    if log_level == "DEBUG":
+        install_rich_tracebacks(show_locals=True)
 
     # Add a filter to redact passwords from URLs to all handlers of the root logger
     password_redact_filter = RedactPasswordFilter()
